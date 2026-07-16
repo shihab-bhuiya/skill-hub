7ecdf8f73c89f382189e0713ddcb301c258e586b
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-07-16 08:50:23 +0600

    Fix TypeScript build error

--- src/lib/api/user.ts
+++
@@ -1,9 +0,0 @@
-// src/services/user.service.ts
-
-import { User } from "../models/user.model";
-
-export const getAllUsers = async () => {
-    const users = await User.find().select("-password");
-
-    return users;
-};
