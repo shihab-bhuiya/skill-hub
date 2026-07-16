454c7ea4164e6799d5a3b3bebfe63c0acc9b4514
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-07-16 08:07:22 +0600

    Connect feature page into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import FeaturedCourses from "@/components/HomePage/FeaturedCourses";
 import Hero from "@/components/HomePage/Hero";
 
 
 export default function Home() {
   return (
+    <div>
+
 <Hero/>
+<FeaturedCourses/>
+
+
+    </div>
   );
 }
