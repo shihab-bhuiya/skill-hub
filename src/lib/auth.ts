4c022c3239bff9b0fbde0e68d38e02350f4b3847
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-07-16 09:10:45 +0600

    Change auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,22 +1,32 @@
 import { betterAuth } from "better-auth";
-import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-const uri: string | undefined = process.env.MONGO_URI;
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_URI;
+
 if (!uri) {
     throw new Error("MONGO_URI is not defined");
 }
+
+if (!process.env.BETTER_AUTH_URL) {
+    throw new Error("BETTER_AUTH_URL is not defined");
+}
+
 const client = new MongoClient(uri);
+
 await client.connect();
+
 const db = client.db("skill-Hub");
 
 export const auth = betterAuth({
     database: mongodbAdapter(db, {
-        // Optional: if you don't provide a client, database transactions won't be enabled.
-        client
+        client,
     }),
+
     emailAndPassword: {
         enabled: true,
     },
+
     user: {
         additionalFields: {
             role: {
@@ -24,8 +34,7 @@ export const auth = betterAuth({
                 required: true,
                 defaultValue: "user",
                 input: false,
-            }
-        }
-    }
-
+            },
+        },
+    },
 });
